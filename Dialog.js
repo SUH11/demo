63cd9433a5425dfd533ee74895b9370678c41baa
SUH <1067489058@qq.com>
2017-05-13 16:09:33 +0800

    简化Dialog.js

--- Dialog.js
+++ Dialog.js
@@ -45,176 +45,173 @@ function Dialog() {
 
 }
 
-Dialog.prototype.init = function( opts ) {
-	extend( this.settings, opts );
-	// 设置包裹层，好设置动画
-	this.oWrap = document.createElement('div');
-	this.oWrap.className = 'dialog_wrap';
-	document.body.appendChild(this.oWrap);
+Dialog.prototype = {
+	// 原型链
+	constructor : Dialog,
+	init : function( opts ) {
+		extend( this.settings, opts );
+		// 设置包裹层，好设置动画
+		this.oWrap = document.createElement('div');
+		this.oWrap.className = 'dialog_wrap';
+		document.body.appendChild(this.oWrap);
 
-	this.oDialog = document.createElement('div');
-	this.oWrap.appendChild(this.oDialog);
+		this.oDialog = document.createElement('div');
+		this.oWrap.appendChild(this.oDialog);
 
-	this.oDialog.className = 'dialog';
-	this.oDialog.innerHTML = '<span class="dialog_title">'+ this.settings.title +'<button>&#215;</button></span><p>'+ this.settings.content+'</p>';
+		this.oDialog.className = 'dialog';
+		this.oDialog.innerHTML = '<span class="dialog_title">'+ this.settings.title +'<button>&#215;</button></span><p>'+ this.settings.content+'</p>';
 
-	
-
-
-	this.oClose = this.oDialog.getElementsByTagName('button')[0];
-
-	// 查看是否有选项
-
-	if ( opts.answer || opts.sure ) {
-		var hr = document.createElement('hr');
-		var oAnswer = document.createElement('div');
-		oAnswer.className = 'answer';
-
-		var oButton = document.createElement('button');
 		
 
-		if ( opts.answer ) {
 
-			oButton.innerHTML = this.settings.answer.no;
-			oAnswer.appendChild(oButton);
+		this.oClose = this.oDialog.getElementsByTagName('button')[0];
+
+		// 查看是否有选项
+
+		if ( opts.answer || opts.sure ) {
+			var hr = document.createElement('hr');
+			var oAnswer = document.createElement('div');
+			oAnswer.className = 'answer';
+
 			var oButton = document.createElement('button');
-			oButton.innerHTML = this.settings.answer.yes;
-			oAnswer.appendChild( oButton );
-
 			
-		} else if ( opts.sure ) {
-			oButton.innerHTML = this.settings.sure;
-			oAnswer.appendChild(oButton);
+
+			if ( opts.answer ) {
+
+				oButton.innerHTML = this.settings.answer.no;
+				oAnswer.appendChild(oButton);
+				var oButton = document.createElement('button');
+				oButton.innerHTML = this.settings.answer.yes;
+				oAnswer.appendChild( oButton );
+
+				
+			} else if ( opts.sure ) {
+				oButton.innerHTML = this.settings.sure;
+				oAnswer.appendChild(oButton);
+			}
+			
+			this.oDialog.appendChild(hr);
+			this.oDialog.appendChild(oAnswer);
+
+			this.aAnswer = oAnswer.getElementsByTagName('button');
+
+			this.return();
+
+		} 
+		// 设置初始样式
+		this.oDialog.style.width = this.settings.width + 'px';
+		
+		this.oWrap.style.left = (document.documentElement.clientWidth - parseInt(getStyle(this.oDialog, 'width')) )/2 + 'px';
+		this.oWrap.style.top = '20px';
+		this.oWrap.style.height = parseInt( getStyle( this.oDialog, 'height')) + 'px';
+
+		// 是否设置拖拽
+		if ( this.settings.drag ) {
+			this.drag();
+		}			
+
+		this.oCloseClick();
+
+	},
+	// 拖拽
+	drag : function() {
+		var _this = this;
+		this.oWrap.onmousedown = function( ev ) {
+			// document.style.cursor = 'move';
+			this.style.cursor = 'cursor';
+			var ev = ev || event;
+			_this.dragDown(ev);
+
+			document.onmousemove = function(ev) {
+				// _this.oWrap.style.cursor = 'move';
+				_this.dragMove(ev);
+			}
+
+			document.onmouseup = function() {
+				_this.dragUp();
+
+			}
+
+			return false;
 		}
 		
-		this.oDialog.appendChild(hr);
-		this.oDialog.appendChild(oAnswer);
-
-		this.aAnswer = oAnswer.getElementsByTagName('button');
-
-		this.return();
-
-	} 
-	// 设置初始样式
-	this.oDialog.style.width = this.settings.width + 'px';
-	
-	this.oWrap.style.left = (document.documentElement.clientWidth - parseInt(getStyle(this.oDialog, 'width')) )/2 + 'px';
-	this.oWrap.style.top = '20px';
-	this.oWrap.style.height = parseInt( getStyle( this.oDialog, 'height')) + 'px';
-
-	// 是否设置拖拽
-	if ( this.settings.drag ) {
-		this.drag();
-	}			
-
-	this.oCloseClick();
-
-}
-
-// 拖拽
-Dialog.prototype.drag = function() {
-	var _this = this;
-	this.oWrap.onmousedown = function( ev ) {
-		// document.style.cursor = 'move';
-		this.style.cursor = 'cursor';
+	},
+	dragDown : function(ev) {
+		// this.oWrap.style.cursor = 'move';
+		var pos = getValue(this.oWrap);
+		this.disX = ev.clientX - pos.l;
+		this.disY = ev.clientY - pos.t;
+	},
+	dragMove : function(ev) {
 		var ev = ev || event;
-		_this.dragDown(ev);
+		var L = ev.clientX - this.disX;
+		var T = ev.clientY - this.disY;
+		var maxL = document.documentElement.clientWidth - this.oWrap.offsetWidth;
+		var maxT = document.documentElement.clientHeight - this.oWrap.offsetHeight;
 
-		document.onmousemove = function(ev) {
-			// _this.oWrap.style.cursor = 'move';
-			_this.dragMove(ev);
+		// 限制范围的拖拽
+		if ( L < 0 ) {
+			L = 0;
+		} else if ( L > maxL ) {
+			L = maxL;
+		}
+		if ( T < 0 ) {
+			T = 0; 
+		} else if ( T > maxT ) {
+			T = maxT;
 		}
 
-		document.onmouseup = function() {
-			_this.dragUp();
+		this.oWrap.style.left = L + 'px';
+		this.oWrap.style.top = T + 'px';
+	},
+	dragUp : function() {
+		this.oWrap.style.cursor = 'default';
+		document.onmouseup = document.onmousemove = null;
 
-		}
-
-		return false;
-	}
-	
-}
-
-Dialog.prototype.dragDown = function(ev) {
-	// this.oWrap.style.cursor = 'move';
-	var pos = getValue(this.oWrap);
-	this.disX = ev.clientX - pos.l;
-	this.disY = ev.clientY - pos.t;
-}
-
-Dialog.prototype.dragMove = function(ev) {
-	var ev = ev || event;
-	var L = ev.clientX - this.disX;
-	var T = ev.clientY - this.disY;
-	var maxL = document.documentElement.clientWidth - this.oWrap.offsetWidth;
-	var maxT = document.documentElement.clientHeight - this.oWrap.offsetHeight;
-
-	// 限制范围的拖拽
-	if ( L < 0 ) {
-		L = 0;
-	} else if ( L > maxL ) {
-		L = maxL;
-	}
-	if ( T < 0 ) {
-		T = 0; 
-	} else if ( T > maxT ) {
-		T = maxT;
-	}
-
-	this.oWrap.style.left = L + 'px';
-	this.oWrap.style.top = T + 'px';
-}
-
-Dialog.prototype.dragUp = function() {
-	this.oWrap.style.cursor = 'default';
-	document.onmouseup = document.onmousemove = null;
-
-}
-
-// 点击x关闭
-Dialog.prototype.oCloseClick = function(){
-	var _this = this;
-	this.oClose.onclick = function() {
-		_this.close();
-	}
-	
-}
-
-// 直接关闭
-Dialog.prototype.close = function() {
-	
-	var _this = this;
-	
-	var timer = setInterval(function(){
-		var iH = parseInt(getStyle(_this.oWrap, 'height'));
-		var iSpeed = -iH/20;
-		iSpeed = Math.floor(iSpeed);
-
-		if ( iH <= Math.abs(iSpeed) ) {
-			_this.oWrap.style.height = '0px';
-			clearInterval( timer );
-		} else {
-			_this.oWrap.style.height = iH + iSpeed + 'px';
-		}
-
-	}, 10);
-}
-
-// 设置对话框的返回值
-Dialog.prototype.return = function() {
-	var _this = this;			
-
-	this.aAnswer[0].onclick = function() {
-		_this.flag = false;
-		_this.close();
-	}
-	if ( this.aAnswer.length > 1 ) {
-		this.aAnswer[1].onclick = function() {
-			_this.flag = true;
+	},
+	// 点击x关闭
+	oCloseClick : function(){
+		var _this = this;
+		this.oClose.onclick = function() {
 			_this.close();
 		}
+		
+	},
+	// 直接关闭
+	close : function() {
+	
+		var _this = this;
+		
+		var timer = setInterval(function(){
+			var iH = parseInt(getStyle(_this.oWrap, 'height'));
+			var iSpeed = -iH/20;
+			iSpeed = Math.floor(iSpeed);
+
+			if ( iH <= Math.abs(iSpeed) ) {
+				_this.oWrap.style.height = '0px';
+				clearInterval( timer );
+			} else {
+				_this.oWrap.style.height = iH + iSpeed + 'px';
+			}
+
+		}, 10);
+	},
+	// 设置对话框的返回值
+	return : function() {
+		var _this = this;			
+
+		this.aAnswer[0].onclick = function() {
+			_this.flag = false;
+			_this.close();
+		}
+		if ( this.aAnswer.length > 1 ) {
+			this.aAnswer[1].onclick = function() {
+				_this.flag = true;
+				_this.close();
+			}
+		}
 	}
-}
+};
 
 function getValue( obj ) {
 	var pos = {
